75e3a3b342f8f4783d3aff16ff6884397b561668
Jamie Smith <jamie@jsmth.co.uk>
2025-06-03 23:27:04 +0100

    Use PostgreSQL UUID columns in Alembic migrations

--- api/alembic/versions/0002_create_film_uses_table.py
+++ api/alembic/versions/0002_create_film_uses_table.py
@@ -7,6 +7,7 @@ Create Date: 2024-01-02 00:00:00.000000
 
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 revision = '0002'
 down_revision = '0001'
@@ -17,9 +18,9 @@ depends_on = None
 def upgrade():
     op.create_table(
         'filmuse',
-        sa.Column('id', sa.String(length=36), primary_key=True),
+        sa.Column('id', postgresql.UUID(as_uuid=True), primary_key=True),
         sa.Column('date_used', sa.Date(), nullable=False),
-        sa.Column('film_id', sa.String(length=36), sa.ForeignKey('film.id'), nullable=False),
+        sa.Column('film_id', postgresql.UUID(as_uuid=True), sa.ForeignKey('film.id'), nullable=False),
         sa.Column('camera', sa.String(), nullable=False),
         sa.Column('location', sa.String(), nullable=False),
         sa.Column('developer', sa.String(), nullable=False),
