ffa3a4807e216d6d9de6ac4a982bfcd10237b848
Jamie Smith <jamie@jsmth.co.uk>
2025-06-03 22:16:37 +0100

    Fix session dependency

--- api/app/database.py
+++ api/app/database.py
@@ -1,12 +1,11 @@
 import os
 from sqlmodel import SQLModel, create_engine, Session
 from sqlalchemy.exc import SQLAlchemyError
-from contextlib import contextmanager
+
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./filmtracker.db")
 engine = create_engine(DATABASE_URL, echo=True)
 
-@contextmanager
 def get_session():
     session = Session(engine)
     try:
